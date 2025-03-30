2411d78b50a17751ee8018394a68f2db9328812b
bassg0navy <bassgonavy@gmail.com>
2025-03-30 14:10:45 -0500

    initial commit

---
+++ __main__.py
@@ -0,0 +1,29 @@
+"""Creates simple infrastructure for Pulumi import testing."""
+
+import pulumi
+import pulumi_oci
+import oci
+
+# Parse values from config files
+# TO-DO: (add fallback to environment variables)
+# Prefix to make distinction between values derived from OCI vs Pulumi config
+oci_config = oci.config.from_file(profile_name='TERRAFORM')
+compartment_id = oci_config.get('tenancy')
+namespace = oci_config.get('namespace')
+
+# Create an OCI Object Storage bucket
+bucket = pulumi_oci.objectstorage.Bucket('pulumi-resource-import-bucket',
+    compartment_id=compartment_id,
+    name='pulumi-resource-import-bucket',
+    namespace=namespace,
+    metadata={
+        'Created-By': 'Pulumi',
+        'Purpose': 'Demo'
+    },
+    storage_tier='Standard',
+    versioning='Enabled'
+)
+
+# Export the bucket name and URL
+pulumi.export('bucket_name', bucket.name)
+pulumi.export('bucket_namespace', bucket.namespace)
